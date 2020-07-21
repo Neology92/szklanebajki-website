c8bf0467faf8d67e576171539a87f98a46936018
Neology92 <oskarlegner@gmail.com>
2020-07-21 18:22:56 +0200

    Update meta data

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Szklane Bajki | Sandra Dyrka`,
+    description: `Niezwykłe, naturalne kompozycje w szkle. Sandra Dyrka - florystka zajmująca się głównie pracami w słoikach. Zajrzyj i zanurz się szklanych opowieściach.`,
+    author: `@Neology92`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -15,18 +15,18 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
-    },
+    // {
+    //   resolve: `gatsby-plugin-manifest`,
+    //   options: {
+    //     name: `Szklane Bajki - Sandra Dyrka`,
+    //     short_name: `szklanebajki`,
+    //     start_url: `/`,
+    //     background_color: `#177d0a`,
+    //     theme_color: `#177d0a`,
+    //     display: `minimal-ui`,
+    //     icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+    //   },
+    // },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
