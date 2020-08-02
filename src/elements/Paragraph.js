b18e1d2946a97c7ad71f114ece1fe8ea8c6884b9
Neology92 <oskarlegner@gmail.com>
2020-08-02 12:06:01 +0200

    Update fonts

--- src/elements/Paragraph.js
+++ src/elements/Paragraph.js
@@ -2,7 +2,6 @@ import styled from "styled-components"
 
 const Paragraph = styled.p`
   margin: 20px 0 0;
-  font-family: "Sahitya";
 `
 
 export default Paragraph
--- src/elements/TextHeading.js
+++ src/elements/TextHeading.js
@@ -2,6 +2,5 @@ import styled from "styled-components"
 
 const TextHeading = styled.h2`
   font-size: 4rem;
-  font-family: "League Script";
 `
 export default TextHeading
