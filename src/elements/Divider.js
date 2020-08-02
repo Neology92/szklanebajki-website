32e9e97da75d01b7e09fd22a039613a74bdcaf0e
Neology92 <oskarlegner@gmail.com>
2020-08-02 14:22:16 +0200

    Style divider for mobiles

--- src/elements/Divider.js
+++ src/elements/Divider.js
@@ -2,6 +2,10 @@ import styled from "styled-components"
 
 const Divider = styled.hr`
   width: 100%;
+  border-top: 1px solid #aaa;
+  border-bottom: none;
+  border-right: none;
+  border-left: none;
 `
 
 export default Divider
